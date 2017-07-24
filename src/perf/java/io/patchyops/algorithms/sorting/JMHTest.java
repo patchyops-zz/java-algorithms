13e8550dcf4423cae2f02be3d128ad0acff09f63
rat <patchyops@gmail.com>
2017-07-24 11:39:21 +0530

    First look at JMH

---
+++ src/perf/java/io/patchyops/algorithms/sorting/JMHTest.java
@@ -0,0 +1,13 @@
+package io.patchyops.algorithms.sorting;
+
+import org.openjdk.jmh.annotations.Benchmark;
+
+public class JMHTest {
+    @Benchmark
+    public void testMethod() {
+
+        int a = 1;
+        int b = 2;
+        int sum = a + b;
+    }
+}
